a9cb701f2832bec94f4b9d1596f0c6419519ac9e
richardlt <richard.le.terrier@gmail.com>
2018-01-28 10:22:54 +0100

    feat: Take care of multitouch pins

--- device/gamepad/gamepad.js
+++ device/gamepad/gamepad.js
@@ -37,9 +37,21 @@ module.exports = class Gamepad extends EventEmitter {
   }
 
   _handleData(device) {
+    const mapPins = {};
+    device.config.pins.forEach(pin => { mapPins[pin.number] = pin; });
+
     return data => {
-      device.config.buttons.forEach(button => {
-        const isPressed = (data[button.pin] & 0xff) == button.value;
+      // sort buttons by values and substract value from pin value to detect 
+      // button state for multitouch pins or compare value for monotouch pins
+      device.config.buttons.sort((b1, b2) => {
+        return b2.value - b1.value;
+      }).forEach(button => {
+        const isPressed = mapPins[button.pin].multi ?
+          (data[button.pin] - button.value) >= 0 : data[button.pin] == button.value;
+
+        if (isPressed && mapPins[button.pin].multi) {
+          data[button.pin] -= button.value;
+        }
 
         const currentState = device.states[button.name];
 
