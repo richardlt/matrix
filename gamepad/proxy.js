9f7c2038ee345d85f7e4b981c861ddcd5b02637f
richardlt <richard.le.terrier@gmail.com>
2020-09-22 23:10:48 +0200

    refactor: update gamepad deps

--- gamepad/proxy.js
+++ gamepad/proxy.js
@@ -1,5 +1,5 @@
-const browserSync = require('browser-sync');
-const proxy = require('http-proxy-middleware');
+const browserSync = require("browser-sync");
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
 browserSync.init({
   port: 4002,
@@ -7,11 +7,13 @@ browserSync.init({
   files: ["src/**/*.*", "index.html", "*.js"],
   proxy: {
     target: "localhost:4001",
-    middleware: [proxy("/socket.io/**", {
-      target: 'http://localhost:4000',
-      ws: true,
-      changeOrigin: true
-    })]
+    middleware: [
+      createProxyMiddleware("/socket.io/**", {
+        target: "http://localhost:4000",
+        ws: true,
+        changeOrigin: true,
+      }),
+    ],
   },
-  open: false
+  open: false,
 });
